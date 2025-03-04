550d936a6f7458ed4dd2b524f097aa1e5b0d96c1
changyongmun <changyong@changyongmuns-Laptop.local>
2025-03-04 18:28:13 +0900

    Feat: add profile page layout and eliminate unnecessary image file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  appDir: true,  // App Router 활성화
 };
 
 export default nextConfig;
---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <h2 className="text-">About Page</h2>;
+};
+
+export default AboutPage;
